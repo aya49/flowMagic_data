e37786304416d5f2613f9637036e16ca434e3799
aya43@sfu.ca <aya43@sfu.ca>
2021-01-17 17:37:30 -0800

    updating tensorflow parameter names...

--- Util/feedforwadClassifier.py
+++ Util/feedforwadClassifier.py
@@ -111,7 +111,7 @@ def trainClassifier(trainSample, mode = 'None', i = 0,
     lrate = LearningRateScheduler(step_decay)
     optimizer = keras.optimizers.rmsprop(lr = 0.0)
     
-    ls = 'sparse_categorical_crossentropy' if numClasses > 1 else 'binary_crossentropy'
+    ls = 'sparse_categorical_crossentropy' if numClasses > 2 else 'binary_crossentropy'
     net.compile(optimizer = optimizer,
                 loss = ls)
     net.fit(x_train, y_train, nb_epoch = 80, batch_size = 128, shuffle = True,
@@ -215,7 +215,7 @@ def plotHidden(trainSample, testSample, mode = 'None', i = 0,
     lrate = LearningRateScheduler(step_decay)
     optimizer = keras.optimizers.rmsprop(lr = 0.0)
     
-    ls = 'sparse_categorical_crossentropy' if numClasses > 1 else 'binary_crossentropy'
+    ls = 'sparse_categorical_crossentropy' if numClasses > 2 else 'binary_crossentropy'
     net.compile(optimizer = optimizer, 
                 loss = ls)
     net.fit(x_train, y_train, nb_epoch = 80, batch_size = 128, shuffle = True,
