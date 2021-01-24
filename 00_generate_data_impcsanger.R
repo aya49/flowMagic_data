a776ff622d76411be397c011ea48a955702804ec
aya43@sfu.ca <aya43@sfu.ca>
2021-01-24 11:47:25 -0800

    update

--- 00_generate_data_impcsanger.R
+++ 00_generate_data_impcsanger.R
@@ -5,7 +5,7 @@
 
 
 ## set directory, load packages, set parallel ####
-no_cores <- 2#parallel::detectCores() - 5
+no_cores <- 20#parallel::detectCores() - 5
 # root <- "/mnt/FCS_local2/Brinkman group/Alice/flowMagic_data"
 # root <- "/home/ayue/projects/flowMagic_data"
 root <- "/mnt/f/Brinkman group/current/Alice/flowMagic_data"
