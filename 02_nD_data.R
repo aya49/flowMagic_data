659b9213652cf0e077dcab6c4d41f8a01968c97b
aya43@sfu.ca <aya43@sfu.ca>
2021-01-28 11:58:40 -0800

    running

--- 02_nD_data.R
+++ 02_nD_data.R
@@ -5,10 +5,10 @@
 
 
 ## set directory, load packages, set parallel ####
-no_cores <- 10#parallel::detectCores() - 5
+no_cores <- 15#parallel::detectCores() - 5
 # root <- "/mnt/FCS_local2/Brinkman group/Alice/flowMagic_data"
-root <- "/home/ayue/projects/flowMagic_data"
-# root <- "/mnt/FCS_local3/backup/Brinkman group/current/Alice/flowMagic_data"
+# root <- "/home/ayue/projects/flowMagic_data"
+root <- "/mnt/FCS_local3/backup/Brinkman group/current/Alice/flowMagic_data"
 source(paste0(root,"/src/RUNME.R"))
 
 
@@ -25,15 +25,16 @@ xn_files <- list.files(xn_dir, recursive=TRUE, full.names=TRUE, pattern=".csv.gz
 ## START ####
 start <- Sys.time()
 
-res <- plyr::llply(xn_files, function(xn_file) {
-  xn <- data.table::fread(xn_file, data.table=FALSE)
+loop_ind <- loop_ind_f(sample(xn_files), no_cores)
+res <- plyr::llply(loop_ind, function(xn_fs) { plyr::llply(xn_fs, function(xn_f) {
+  xn <- data.table::fread(xn_f, data.table=FALSE)
   tx <- Rtsne::Rtsne(xn[!duplicated(xn),,drop=FALSE])$Y
-  tx_file <- gs_xr(xn_file)
+  tx_file <- gs_xr(xn_f)
   tx_dir <- stringr::str_split(tx_file,"/")[[1]]
   tx_dir <- paste0(tx_dir[-length(tx_dir)],collapse="/")
   colnames(tx) <- c("tsne 1", "tsne 2")
   write.table(tx, file=gzfile(tx_file), sep=",", row.names=FALSE, col.names=TRUE)
-}, .parallel=TRUE)
+}) }, .parallel=TRUE)
 time_output(start)
 
 
