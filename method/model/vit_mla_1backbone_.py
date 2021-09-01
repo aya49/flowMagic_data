5de1337249f5641e4a7bcac4ef5f6500013e113e
aya43@sfu.ca <aya43@sfu.ca>
2021-08-31 17:28:16 -0700

    minor fixing

--- method/model/vit_mla_1backbone_.py
+++ method/model/vit_mla_1backbone_.py
@@ -194,3 +194,144 @@ class Conv_MLA(nn.Module):
 
         return mla_p2, mla_p3, mla_p4, mla_p5
 
+
+@BACKBONES.register_module()
+class VIT_MLA(nn.Module):
+    """ Vision Transformer with support for patch or hybrid CNN input stage
+    """
+
+    def __init__(self, model_name='vit_large_patch16_384', img_size=384, patch_size=16, in_chans=3, embed_dim=1024, depth=24,
+                 num_heads=16, num_classes=19, mlp_ratio=4., qkv_bias=True, qk_scale=None, drop_rate=0.1, attn_drop_rate=0.,
+                 drop_path_rate=0., hybrid_backbone=None, norm_layer=partial(nn.LayerNorm, eps=1e-6), norm_cfg=None,
+                 pos_embed_interp=False, random_init=False, align_corners=False, mla_channels=256,
+                 mla_index=(5, 11, 17, 23), **kwargs):
+        super(VIT_MLA, self).__init__(**kwargs)
+        self.model_name = model_name
+        self.img_size = img_size
+        self.patch_size = patch_size
+        self.in_chans = in_chans
+        self.embed_dim = embed_dim
+        self.depth = depth
+        self.num_heads = num_heads
+        self.num_classes = num_classes
+        self.mlp_ratio = mlp_ratio
+        self.qkv_bias = qkv_bias
+        self.qk_scale = qk_scale
+        self.drop_rate = drop_rate
+        self.attn_drop_rate = attn_drop_rate
+        self.drop_path_rate = drop_path_rate
+        self.hybrid_backbone = hybrid_backbone
+        self.norm_layer = norm_layer
+        self.norm_cfg = norm_cfg
+        self.pos_embed_interp = pos_embed_interp
+        self.random_init = random_init
+        self.align_corners = align_corners
+        self.mla_channels = mla_channels
+        self.mla_index = mla_index
+
+        self.num_stages = self.depth
+        self.out_indices = tuple(range(self.num_stages))
+
+        if self.hybrid_backbone is not None:
+            self.patch_embed = HybridEmbed(
+                self.hybrid_backbone, img_size=self.img_size, in_chans=self.in_chans, embed_dim=self.embed_dim)
+        else:
+            self.patch_embed = PatchEmbed(
+                img_size=self.img_size, patch_size=self.patch_size, in_chans=self.in_chans, embed_dim=self.embed_dim)
+        self.num_patches = self.patch_embed.num_patches
+
+        self.cls_token = nn.Parameter(torch.zeros(1, 1, self.embed_dim))
+        self.pos_embed = nn.Parameter(torch.zeros(
+            1, self.num_patches + 1, self.embed_dim))
+        self.pos_drop = nn.Dropout(p=self.drop_rate)
+
+        dpr = [x.item() for x in torch.linspace(0, self.drop_path_rate, self.depth)]  # stochastic depth decay rule
+        self.blocks = nn.ModuleList([
+            Block(
+                dim=self.embed_dim, num_heads=self.num_heads, 
+                mlp_ratio=self.mlp_ratio, 
+                qkv_bias=self.qkv_bias, qk_scale=self.qk_scale,
+                drop=self.drop_rate, attn_drop=self.attn_drop_rate, drop_path=dpr[i], 
+                norm_layer=self.norm_layer)
+            for i in range(self.depth)])
+
+        self.mla = Conv_MLA(in_channels=self.embed_dim,
+                            mla_channels=self.mla_channels, norm_cfg=self.norm_cfg)
+
+        self.norm_0 = norm_layer(self.embed_dim)
+        self.norm_1 = norm_layer(self.embed_dim)
+        self.norm_2 = norm_layer(self.embed_dim)
+        self.norm_3 = norm_layer(self.embed_dim)
+
+        # NOTE as per official impl, we could have a pre-logits representation dense layer + tanh here
+        # self.repr = nn.Linear(embed_dim, representation_size)
+        # self.repr_act = nn.Tanh()
+
+        trunc_normal_(self.pos_embed, std=.02)
+        trunc_normal_(self.cls_token, std=.02)
+        # self.apply(self._init_weights)
+
+    def init_weights(self, pretrained=None):
+        # nn.init.normal_(self.pos_embed, std=0.02)
+        # nn.init.zeros_(self.cls_token)
+
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                trunc_normal_(m.weight, std=.02)
+                if isinstance(m, nn.Linear) and m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.LayerNorm):
+                nn.init.constant_(m.bias, 0)
+                nn.init.constant_(m.weight, 1.0)
+
+        # if not self.random_init:
+        #     self.default_cfg = default_cfgs[self.model_name]
+
+        #     if self.model_name in ['vit_small_patch16_224', 'vit_base_patch16_224']:
+        #         load_pretrained(self, num_classes=self.num_classes, in_chans=self.in_chans, pos_embed_interp=self.pos_embed_interp,
+        #                         num_patches=self.patch_embed.num_patches, align_corners=self.align_corners, filter_fn=self._conv_filter)
+        #     else:
+        #         load_pretrained(self, num_classes=self.num_classes, in_chans=self.in_chans, pos_embed_interp=self.pos_embed_interp,
+        #                         num_patches=self.patch_embed.num_patches, align_corners=self.align_corners)
+        # else:
+        #     print('Initialize weight randomly')
+
+    @property
+    def no_weight_decay(self):
+        return {'pos_embed', 'cls_token'}
+
+    def _conv_filter(self, state_dict, patch_size=16):
+        """ convert patch embedding weight from manual patchify + linear proj to conv"""
+        out_dict = {}
+        for k, v in state_dict.items():
+            if 'patch_embed.proj.weight' in k:
+                v = v.reshape((v.shape[0], 3, patch_size, patch_size))
+            out_dict[k] = v
+        return out_dict
+
+    def forward(self, x):
+        B = x.shape[0]
+        x = self.patch_embed(x)
+        x = x.flatten(2).transpose(1, 2)
+
+        # stole cls_tokens impl from Phil Wang, thanks
+        cls_tokens = self.cls_token.expand(B, -1, -1)
+        x = torch.cat((cls_tokens, x), dim=1)
+        x = x + self.pos_embed
+        x = x[:, 1:]
+        x = self.pos_drop(x)
+
+        outs = []
+        for i, blk in enumerate(self.blocks):
+            x = blk(x)
+            if i in self.out_indices:
+                outs.append(x)
+
+        c6 = self.norm_0(outs[self.mla_index[0]])
+        c12 = self.norm_1(outs[self.mla_index[1]])
+        c18 = self.norm_2(outs[self.mla_index[2]])
+        c24 = self.norm_3(outs[self.mla_index[3]])
+
+        p6, p12, p18, p24 = self.mla(c6, c12, c18, c24)
+
+        return (p6, p12, p18, p24)
