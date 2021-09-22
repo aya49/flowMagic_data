41b5077c4fb95f9b97095806175a828762a23f2f
aya43@sfu.ca <aya43@sfu.ca>
2021-09-21 18:49:42 -0700

    debug transform

--- method/transform.py
+++ method/transform.py
@@ -1,37 +1,37 @@
-import torchvision.transforms as transforms
-import torchvision.transforms.functional as transfunc
+import torchvision.transforms as tr
+import torchvision.transforms.functional as tf
 
 # for training with training samples
 def transform_A(x, y, s=None):
     # Random rotation
-    deg = 90 * (transforms.RandomRotation((0,359)).get_params() // 90)
+    deg = 90 * (tr.RandomRotation((0,359)).get_params() // 90)
     if deg != 0:
-        x = transfunc.rotate(x, deg)
-        y = transfunc.rotate(y, deg)
+        x = tf.rotate(x, deg)
+        y = tf.rotate(y, deg)
 
     # Random horizontal flipping
     if random.random() > 0.5:
-        x = transfunc.hflip(x)
-        y = transfunc.hflip(y)
+        x = tf.hflip(x)
+        y = tf.hflip(y)
 
     # Random vertical flipping
     if random.random() > 0.5:
-        x = transfunc.vflip(x)
-        y = transfunc.vflip(y)
+        x = tf.vflip(x)
+        y = tf.vflip(y)
     
     # Random resize
-    params = transforms.RandomResizedCrop(size=s, scale=(0.8, 1.0), ratio=(0.75, 1.33)).get_params()
-    x = transfunc.crop(x, *params)
-    y = transfunc.crop(x, *params)
+    params = tr.RandomResizedCrop(size=s, scale=(0.8, 1.0), ratio=(0.75, 1.33)).get_params()
+    x = tf.crop(x, *params)
+    y = tf.crop(x, *params)
     
     return x, y
 
 # for meta training with reference samples
 def transform_B(x, y, s):
     # Random resize
-    params = transforms.RandomResizedCrop(size=s, scale=(0.8, 1.0), ratio=(0.75, 1.33)).get_params()
-    x = transfunc.crop(x, *params)
-    y = transfunc.crop(x, *params)
+    params = tr.RandomResizedCrop(size=s, scale=(0.8, 1.0), ratio=(0.75, 1.33)).get_params()
+    x = tf.crop(x, *params)
+    y = tf.crop(x, *params)
 
     return x, y
 
@@ -41,7 +41,6 @@ transform_dict = {
     'B': transform_B
 }
 
-
 transform_names = list()
-for name, dict_ in transforms_dict.items():
+for name, dict_ in transform_dict.items():
     transform_names.append(name)
