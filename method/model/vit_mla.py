67e723d53067d41f0ebe8f68cba1fb52e6e45893
aya43@sfu.ca <aya43@sfu.ca>
2021-09-30 18:32:42 -0700

    setr

--- method/model/vit_mla.py
+++ method/model/vit_mla.py
@@ -1,6 +1,9 @@
 import mmcv
-from mmseg.apis import init_segmentor, inference_segmentor, init_cfg
+from mmseg.apis import init_segmentor#, inference_segmentor, init_cfg
 
-cfg = mmcv.Config.fromfile('vit_mla_cfg.py')
-# cfg = mmcv.Config.fromfile('/home/aya43/flowMagic_data/src/method/model/vit_mla_cfg.py')
-model = init_segmentor(cfg)
+def model_setr(opts):
+    cfg = mmcv.Config.fromfile('vit_mla_cfg.py')
+    # cfg = mmcv.Config.fromfile('/home/aya43/flowMagic_data/src/method/model/vit_mla_cfg.py')
+    model = init_segmentor(cfg)
+
+    return model
