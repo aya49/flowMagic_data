06b59fbcf727e0eab5e70aeb9d19f275f0e8b654
aya43@sfu.ca <aya43@sfu.ca>
2021-10-01 16:41:13 -0700

    bug

--- method/model/__init__.py
+++ method/model/__init__.py
@@ -1,5 +1,28 @@
-from .vit_mla import model_setr
-from .unet import model_unet
+import torch
+import mmcv
+from mmseg.apis import init_segmentor#, inference_segmentor, init_cfg
+
+def model_unet(opts):
+    cfg = mmcv.Config.fromfile('unet_cfg.py')
+    # cfg = mmcv.Config.fromfile('/home/aya43/flowMagic_data/src/method/model/unet_cfg.py')
+    
+    if torch.cuda.is_available():
+        model = init_segmentor(cfg, device='cuda:0')
+    else:
+        model = init_segmentor(cfg, device='cpu')
+
+    return model
+
+def model_setr(opts):
+    cfg = mmcv.Config.fromfile('vit_mla_cfg.py')
+    # cfg = mmcv.Config.fromfile('/home/aya43/flowMagic_data/src/method/model/vit_mla_cfg.py')
+    
+    if torch.cuda.is_available():
+        model = init_segmentor(cfg, device='cuda:0')
+    else:
+        model = init_segmentor(cfg, device='cpu')
+
+    return model
 
 model_dict = {
     'setr': model_setr,
