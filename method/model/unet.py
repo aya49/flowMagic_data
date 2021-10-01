ef3342142443ddd2ce098bdd8f26211e1a151025
aya43@sfu.ca <aya43@sfu.ca>
2021-10-01 16:36:20 -0700

    bug

--- method/model/unet.py
+++ method/model/unet.py
@@ -2,7 +2,7 @@ import torch
 import mmcv
 from mmseg.apis import init_segmentor#, inference_segmentor, init_cfg
 
-def model_setr(opts):
+def model_unet(opts):
     cfg = mmcv.Config.fromfile('unet_cfg.py')
     # cfg = mmcv.Config.fromfile('/home/aya43/flowMagic_data/src/method/model/vit_mla_cfg.py')
     
